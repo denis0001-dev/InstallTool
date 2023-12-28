ffa9fc699aa9792daba936bd522a9ee42d77b2ed
denis0001 <denis0001minecraft@gmail.com>
2023-12-28 20:41:17 +0300

    Create install.sh

---
+++ appimage/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [[ $1 == *.AppImage ]] && [[ -e $1 ]]
+then
+  &>/dev/null
+else
+  echo "Error: the provided file is not an AppImage."
+  exit 1
+fi
+# Making the temporary installation dir
+echo "Extracting the AppImage to the temporary folder..."
+mkdir /tmp/appimage-install
+cd /tmp/appimage-install
+$1 --appimage-extract 1>/dev/null
+cd ./squashfs-root
+# Copying the AppImage to the installation dir
+echo "Copying the AppImage to the installation dir..."
+mkdir ~/AppImages
+cp $1 ~/AppImages/$1
+# Generating the desktop file
+echo "Generating the desktop file..."
+desktopfile=$(find . -type f -name '*.desktop')
+source <(grep = $desktopfile | tr -d ' ')
+echo "[Desktop Entry]" >> /usr/share/applications/$desktopfile
+echo "Name=$Name" >> /usr/share/applications/$desktopfile
+echo "Exec=sh -c ~/AppImages/$1" >> /usr/share/applications/$desktopfile
+echo "Terminal=false" >> /usr/share/applications/$desktopfile
+echo "Type=Application" >> /usr/share/applications/$desktopfile
+echo "Icon=$Icon" >> /usr/share/applications/$desktopfile
+echo "StartupWMClass=$StartupWMClass" >> /usr/share/applications/$desktopfile
+echo "Comment=$Comment" >> /usr/share/applications/$desktopfile
+echo "Categories=$Categories" >> /usr/share/applications/$desktopfile
+# Generating the uninstall script
+mkdir ~/AppImages/uninstall
+echo "#!/bin/bash" >> ~/AppImages/uninstall/$Name.sh
+echo "# Uninstall script for $Name." >> ~/AppImages/uninstall/$Name.sh
+echo "if [ "$EUID" -ne 0 ]" >> ~/AppImages/uninstall/$Name.sh
+echo "then" >> ~/AppImages/uninstall/$Name.sh
+echo "  echo 'Error: you must run this as root.'" >> ~/AppImages/uninstall/$Name.sh
+echo "fi" >> ~/AppImages/uninstall/$Name.sh
+echo "rm /usr/share/applications/$desktopfile" >> ~/AppImages/uninstall/$Name.sh
+echo "rm ~/AppImages/$1" >> ~/AppImages/uninstall/$Name.sh
+echo "rm ~/AppImages/uninstall/$Name.sh" >> ~/AppImages/uninstall/$Name.sh
+echo "echo 'Done.'" >> ~/AppImages/uninstall/$Name.sh
