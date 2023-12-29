aaf03edbe893355bde439de32556bc49a8359e03
denis0001 <denis0001minecraft@gmail.com>
2023-12-29 11:16:45 +0300

    Update install.sh

--- appimage/install.sh
+++ appimage/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Checking that the specified file exists and it's an AppImage
 if [[ $1 == *.AppImage ]] && [[ -e $1 ]]
 then
   &>/dev/null
@@ -6,14 +7,18 @@ else
   echo "Error: the provided file is not an AppImage."
   exit 1
 fi
+# Checking that this program was ran as sudo, not directly as root
 if [[ $(env | grep SUDO_USER) = "SUDO_USER=root" ]]
 then
   echo "Error: root user is not supported! Use sudo installtool appimage ... instead."
   exit 1
 fi
+# Variables
+source <(grep = $desktopfile | tr -d ' ')
 file_path=$(realpath $1)
 file_name=$(basename $1)
 home_path="/home/$(echo $(env | grep SUDO_USER) | sed 's/SUDO_USER=//')"
+icon_path=$(realpath $(find -L . -type f -wholename "./squashfs-root/$Icon*" | grep -e '.png' -e '.svg'))
 # Making the temporary installation dir
 echo "Extracting the AppImage to the temporary folder..."
 mkdir /tmp/appimage-install
@@ -25,16 +30,18 @@ cd ./squashfs-root
 echo "Copying the AppImage to the installation dir..."
 mkdir $home_path/AppImages
 cp $file_path $home_path/AppImages/$file_name
+# Copying the icon
+mkdir $home_path/AppImages/icons
+cp $icon_path $home_path/AppImages/icons/$(basename $icon_path)
 # Generating the desktop file
 echo "Generating the desktop file..."
 desktopfile=$(basename $(find . -type f -name '*.desktop'))
-source <(grep = $desktopfile | tr -d ' ')
 echo "[Desktop Entry]" >> /usr/share/applications/$desktopfile
 echo $(cat ./$desktopfile | grep 'Name=') >> /usr/share/applications/$desktopfile
-echo "Exec=sh -c ~/AppImages/$file_name" >> /usr/share/applications/$desktopfile
+echo "Exec=sh -c $home_path/AppImages/$file_name" >> /usr/share/applications/$desktopfile
 echo "Terminal=false" >> /usr/share/applications/$desktopfile
 echo "Type=Application" >> /usr/share/applications/$desktopfile
-echo "Icon=$Icon" >> /usr/share/applications/$desktopfile
+echo "Icon=$homepath/AppImages/icons/$(basename $icon_path)" >> /usr/share/applications/$desktopfile
 echo "StartupWMClass=$StartupWMClass" >> /usr/share/applications/$desktopfile
 echo $(cat ./$desktopfile | grep 'Comment=') >> /usr/share/applications/$desktopfile
 echo "Categories=$Categories" >> /usr/share/applications/$desktopfile
@@ -49,5 +56,6 @@ echo "fi" >> $home_path/AppImages/uninstall/$Name.sh
 echo "rm /usr/share/applications/$desktopfile" >> $home_path/AppImages/uninstall/$Name.sh
 echo "rm $home_path/AppImages/$file_name" >> $home_path/AppImages/uninstall/$Name.sh
 echo "rm $home_path/AppImages/uninstall/$Name.sh" >> $home_path/AppImages/uninstall/$Name.sh
+echo "rm $home_path/AppImages/icons/$(basename $icon_path)" >> $home_path/AppImages/uninstall/$Name.sh
 echo "echo 'Done.'" >> $home_path/AppImages/uninstall/$Name.sh
 rm -rf /tmp/appimage_install
