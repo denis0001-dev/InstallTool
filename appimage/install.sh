ba103544567670db10dc839bf71d8e49a134e609
denis0001 <denis0001minecraft@gmail.com>
2023-12-29 12:31:05 +0300

    Update install.sh

--- appimage/install.sh
+++ appimage/install.sh
@@ -17,6 +17,7 @@ fi
 file_path=$(realpath $1)
 file_name=$(basename $1)
 home_path="/home/$(echo $(env | grep SUDO_USER) | sed 's/SUDO_USER=//')"
+echo "HOME PATH: $home_path"
 if [[ $home_path = "/home/" ]]
 then
   echo "Error: home path doesn't exist, try again with sudo."
